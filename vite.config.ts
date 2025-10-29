fc7aa6d4a6242b3c1d71d3507155ed4f7b1b27b0
Adam Wang <154560025@qq.com>
2025-10-29 12:31:33 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Use relative paths for Vercel deployment
   server: {
     port: 5173,
   },
